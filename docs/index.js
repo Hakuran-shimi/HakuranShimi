1ed834ae71ee4071469925b62e26a3862657360e
Hakuran-shimi <namaewoshiranai@gmail.com>
2025-01-03 20:52:52 +0900

    Delete docs directory

--- docs/index.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('load', function () {
-    const button = document.getElementById('open-close');
-    const element = document.getElementById('mobile-nav-open');
-    button.addEventListener('click', () => {
-        element.classList.toggle('mobile-nav');
-    });
-});
